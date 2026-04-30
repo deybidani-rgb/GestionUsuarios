30b76799373f9deaca708ccc3a0ef7c1e285abae
deybidani-rgb <deybidani@gmail.com>
2026-04-29 19:40:03 -0500

    ultimos

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "gestion_usuarios");
+$conexion = new mysqli("mysql-deybys.alwaysdata.net", "root", "clase1234", "deybys_gestionusuarios");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
